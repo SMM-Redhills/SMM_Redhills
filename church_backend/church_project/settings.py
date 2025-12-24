1840d1b11c572fe938162a4a27feaf26a489e89c
P ROOSSO <proosso2452@gmail.com>
2025-12-24 22:00:35 +0530

    Add wildcard for trycloudflare.com to ALLOWED_HOSTS

--- church_backend/church_project/settings.py
+++ church_backend/church_project/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.getenv("SECRET_KEY", "django-insecure-default-key")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv("DEBUG", "True") == "True"
 
-ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS", "localhost,127.0.0.1,galaxy-miscellaneous-midnight-starsmerchant.trycloudflare.com,harvard-oral-entirely-pound.trycloudflare.com,raise-stating-accessory-higher.trycloudflare.com").split(",")
+ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS", "localhost,127.0.0.1,.trycloudflare.com,galaxy-miscellaneous-midnight-starsmerchant.trycloudflare.com,harvard-oral-entirely-pound.trycloudflare.com,raise-stating-accessory-higher.trycloudflare.com").split(",")
 
 
 # Application definition
